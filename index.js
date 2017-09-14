b02eadb776c902cb41940e4fac481e1ee4f93690
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-09-14 16:11:00 +0200

    auto claim parliament id

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const fetch = require('node-fetch')
+const wdk = require('wikidata-sdk')
+
+require('dotenv').config()
+
+
+const config = {
+  username: process.env.WIKI_USER,
+  password: process.env.WIKI_PASS,
+  verbose: false
+}
+const wdEdit = require('wikidata-edit')(config)
+
 const wikidata = require('./wikidata.json').results.bindings
 const lobbywatch = require('./lobbywatch.json').data.parliamentarians
 const parlament = require('./parlament.json')
@@ -16,11 +29,39 @@ const wikidataIndex = wikidata.reduce(
   {}
 )
 
+
 lobbywatch.forEach(mp => {
   const wikidataMp = wikidataIndex[mp.parliamentId]
   if (wikidataMp) {
     // console.log('matched', mp.name, mp.id, wikidataMp[0].human.value)
   } else {
-    console.log('missed', mp.name)
+    fetch(wdk.searchEntities({
+      search: mp.name,
+      limit: 3
+    }))
+      .then(response => response.json())
+      .then(json => {
+        console.log('Missed', mp.name, mp.parliamentId)
+        if (json.success) {
+          // console.log('Wikidata Search', json.search.length)
+          // console.log(json.search)
+          const result = json.search[0]
+          if (
+            json.search.length === 1
+          ) {
+            if (result.description === 'Swiss politician') {
+              console.log('- auto match', result.label, mp.name)
+              console.log('-', result.id, 'P1307', mp.parliamentId)
+              wdEdit.claim.add(result.id, 'P1307', mp.parliamentId)
+            } else {
+              console.log('- needs manual check')
+            }
+          } else {
+            console.log('- multi match')
+          }
+        } else {
+          console.log('- no match')
+        }
+      })
   }
 })
