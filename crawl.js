898b99e4dc2f778b516a6e427239cf36feeb1d05
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-09-14 14:37:47 +0200

    order wikidata, mv all fetching to crawl

--- crawl.js
+++ crawl.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
+const wdk = require('wikidata-sdk')
 const { createApolloFetch } = require('apollo-fetch')
 
 const url = 'http://ws-old.parlament.ch/councillors/basicdetails'
@@ -36,3 +37,32 @@ apolloFetch({
     JSON.stringify(response.data.parliamentarians, null, 2),
     'utf8'
   ))
+
+
+const nationalCouncil = 'Q18510612'
+const councilOfStates = 'Q18510613'
+const federalAssembly = 'Q18515554'
+
+const sparql = `
+SELECT DISTINCT ?human ?Swiss_parliament_ID ?humanLabel
+WHERE {  
+  {
+    {?human wdt:P39 wd:${nationalCouncil}.}
+    UNION {?human wdt:P39 wd:${councilOfStates}.}
+    UNION {?human wdt:P39 wd:${federalAssembly}.}
+
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "de". }
+    OPTIONAL { ?human wdt:P1307 ?Swiss_parliament_ID. }
+  }
+}
+ORDER BY ASC(xsd:integer(?Swiss_parliament_ID)) ASC(?humanLabel)
+`
+
+fetch(wdk.sparqlQuery(sparql))
+  .then(response => response.json())
+  .then(json => fs.writeFileSync(
+    'wikidata.json',
+    JSON.stringify(json, null, 2),
+    'utf8'
+  ))
+
